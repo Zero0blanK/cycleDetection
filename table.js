fc79d95b49932ea95f5b615582f950830c546d49
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-08-25 22:36:43 +0800

    Make table get id of new data

--- table.js
+++ table.js
@@ -3,20 +3,31 @@
 function addRow() {
     const data = document.getElementById('dataInput').value.trim();
     const nextNode = document.getElementById('nextNodeInput').value.trim();
+    const table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+    const idArray = Array.from((table.getElementsByTagName('tr'))).map(row=>row.children[0])
+    let idArrayValues = [];
+    if(idArray){
+        idArray.forEach((td)=>{
+            idArrayValues.push(td.firstChild.data)
+        })
+    }
+    const id = table.childElementCount;
+    const previousElement = Array.from(table.getElementsByTagName('tr')).map(row=>row.children[2])
+    const mostRecentNextElement = previousElement[previousElement.length -1]
 
     if (data === '') {
         alert('Please enter some data.');
         return;
     }
-
-    if (nextNode.length > 1) {
-        alert('Next Node should be a single value.');
+    const nextNodeValueInArray = idArrayValues.find((value)=>value === nextNode)
+    if (!(nextNodeValueInArray) && nextNode  != '' && idArrayValues.length > 0) {
+        alert('Node Id Does Not exist');
         return;
     }
 
-    const id = data.charAt(0);
-
-    const table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+    if(mostRecentNextElement &&mostRecentNextElement.innerHTML === 'null'){
+        mostRecentNextElement.innerText = id;
+    }
     const newRow = table.insertRow();
 
     const idCell = newRow.insertCell(0);
@@ -30,3 +41,5 @@ function addRow() {
     document.getElementById('dataInput').value = '';
     document.getElementById('nextNodeInput').value = '';
 }
+
+window.addRow = addRow
